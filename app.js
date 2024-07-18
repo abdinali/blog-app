2976dce210086d96d0885eefa0ce9c9a61873301
Abdinasir Ali <131626624+abdinasir5@users.noreply.github.com>
2024-07-18 09:20:22 -0400

    server

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import expressEjsLayouts from "express-ejs-layouts";
+import mainRoutes from "./server/routes/main.js";
+
+dotenv.config();
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static("public"));
+app.use(expressEjsLayouts);
+app.set('layout', './layouts/boilerplate');
+app.set("view engine", "ejs");
+
+// ROUTES
+app.use("/", mainRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
